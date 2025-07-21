0fd7565cdf855087b318234734f83ff0fbb08b90
Arjumand Ali <arjamandalialiali51214@gmail.com>
2025-07-22 00:56:27 +0500

    Backend/Update: updated register api

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,7 +1,6 @@
 import random
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
 
